c17348dbe2d4f13567b8d9f330aa00a80adca439
PacificD <1484636442@qq.com>
2022-03-23 18:07:51 +0800

    feat(config/resultType): 状态码的更新

--- src/config/resultType.ts
+++ src/config/resultType.ts
@@ -2,19 +2,20 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-03-17 17:41:09
- * @LastEditTime: 2022-03-23 10:56:15
+ * @LastEditTime: 2022-03-23 14:45:44
  * @LastEditors: Pacific_D
  * @Description: 
  * @FilePath: \class-schedule\src\config\resultType.ts
  */
 export enum statusCodeEnum {
     OK = 200,
-    UNEXPECT = 400, //未知错误
-    NEED_AUTH = 401, //需要登录
-    NO_FIND = 404, //找不到相关资源
-    BAD_REQUEST = 403, //请求错误
-    EXISTED = 408, //字段已存在
-    SEVER_ERR = 500, //服务器错误
+    CREATED = 201, //创建成功
+    BAD_REQUEST = 400, //客户端请求的语法错误，服务器无法理解
+    UNAUTHORIZED = 401, //请求要求用户的身份认证
+    FORBIDDEN = 403, //服务器理解请求客户端的请求，但是拒绝执行此请求
+    NOT_FOUND = 404, //找不到相关资源
+    REQUEST_TIME_OUT = 408, //超时
+    INTERNAL_SERVER_ERROR = 500, //服务器错误
 }
 
 export class Result {
@@ -23,7 +24,7 @@ export class Result {
         private message: string) { }
 
     public static success(data: any) {
-        return new Result(200, data, "success");
+        return new Result(statusCodeEnum.OK, data, "success");
     }
 
     public static fail(statusCode: statusCodeEnum, message: string) {
