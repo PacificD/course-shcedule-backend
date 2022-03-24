43881a1a6bb81907f69d66aa46a8b00e8cb0c244
PacificD <1484636442@qq.com>
2022-03-24 23:08:02 +0800

    feat(numberDateMapper): 新增Date映射number

--- src/utils/generateCoursesData.ts
+++ src/utils/generateCoursesData.ts
@@ -2,7 +2,7 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-03-23 21:34:56
- * @LastEditTime: 2022-03-23 21:43:05
+ * @LastEditTime: 2022-03-24 21:45:31
  * @LastEditors: Pacific_D
  * @Description: 
  * @FilePath: \class-schedule\src\utils\generateCoursesData.ts
@@ -11,14 +11,21 @@
 import ICoursesData from "src/courses/type/coursesData";
 import { DateEnum } from "src/courses/type/dateEnum";
 
-export const numberDateMapper = new Map<number, string>([
+export const numberDateMapper = new Map<number| string, string | number>([
     [1, DateEnum.MON],
     [2, DateEnum.TUES],
     [3, DateEnum.WEDNES],
     [4, DateEnum.THURS],
     [5, DateEnum.FRI],
     [6, DateEnum.SATUR],
-    [7, DateEnum.SUN]
+    [7, DateEnum.SUN],
+    [DateEnum.MON, 1],
+    [DateEnum.TUES, 2],
+    [DateEnum.WEDNES, 3],
+    [DateEnum.THURS, 4],
+    [DateEnum.FRI, 5],
+    [DateEnum.SATUR, 6],
+    [DateEnum.SUN, 7],
 ])
 
 export function generateCoursesData(): Array<ICoursesData> {
