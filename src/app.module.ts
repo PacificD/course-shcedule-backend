b8eb9d72e9c2af55b7fc1991a18ad1ed2ce796f6
PacificD <1484636442@qq.com>
2022-03-26 14:27:22 +0800

    feat: 静态资源

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-03-22 22:57:06
- * @LastEditTime: 2022-03-26 11:40:39
+ * @LastEditTime: 2022-03-26 11:47:22
  * @LastEditors: Pacific_D
  * @Description: 
  * @FilePath: \class-schedule\src\app.module.ts
@@ -10,8 +10,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClassifyModule } from './classify/classify.module';
 import { CoursesModule } from './courses/courses.module';
 import { UserModule } from './user/user.module';
@@ -20,7 +18,5 @@ import { UserModule } from './user/user.module';
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public')
   }), ClassifyModule, UserModule, CoursesModule],
-  // controllers: [AppController],
-  // providers: [AppService],
 })
 export class AppModule {}
